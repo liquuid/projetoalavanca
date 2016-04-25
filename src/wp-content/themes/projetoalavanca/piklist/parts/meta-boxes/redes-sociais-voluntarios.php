53785b10b9a8f34c9edf8bfda8b017c92b36f3e4
Fernando Silva <liquuid@gmail.com>
2016-04-25 16:55:32 -0300

    Adicionando custom posttype voluntario

---
+++ src/wp-content/themes/projetoalavanca/piklist/parts/meta-boxes/redes-sociais-voluntarios.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Title: Adicione as redes sociais do voluntário
+Post Type: voluntario_post_type
+ */
+
+piklist('field', array(
+    'type' => 'text'
+    ,'field' => 'facebook'
+    ,'label' => 'Facebook'
+    ,'description' => 'Adicione aqui o perfil do voluntário no Facebook'
+    ,'attributes' => array(
+        'class' => 'text'
+    )
+));
+piklist('field', array(
+    'type' => 'text'
+    ,'field' => 'gplus'
+    ,'label' => 'Google Plus'
+    ,'description' => 'Adicione aqui o perfil do voluntário no Google Plus'
+    ,'attributes' => array(
+        'class' => 'text'
+    )
+));
+
+piklist('field', array(
+    'type' => 'text'
+    ,'field' => 'pinterest'
+    ,'label' => 'Pinterest'
+    ,'description' => 'Adicione aqui o perfil do voluntário no Pinterest'
+    ,'attributes' => array(
+        'class' => 'text'
+    )
+));
+piklist('field', array(
+    'type' => 'text'
+    ,'field' => 'linkedin'
+    ,'label' => 'Linkedin'
+    ,'description' => 'Adicione aqui o perfil do voluntário no Linkedin'
+    ,'attributes' => array(
+        'class' => 'text'
+    )
+));
+piklist('field', array(
+    'type' => 'text'
+    ,'field' => 'twitter'
+    ,'label' => 'Twitter'
+    ,'description' => 'Adicione aqui o perfil do voluntário no Twitter'
+    ,'attributes' => array(
+        'class' => 'text'
+    )
+));
