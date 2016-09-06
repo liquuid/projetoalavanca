d0e3846a99bada4c1a6a1db39dddfe574d1f9893
Fernando Silva <liquuid@gmail.com>
2016-09-06 11:00:44 -0300

    criando página de template para o frontend para exibir apenas os filhos

--- src/wp-content/themes/projetoalavanca/front-page.php
+++ src/wp-content/themes/projetoalavanca/front-page.php
@@ -1,5 +1,9 @@
-<?php get_template_part('templates/page', 'header'); ?>
-
+<?php
+/*
+* Template Name: Frontpage Template
+*/
+?>
+<?php //get_template_part('templates/page', 'header'); ?>
 <?php if (!have_posts()) : ?>
   <div class="alert alert-warning">
     <?php _e('Sorry, no results were found.', 'sage'); ?>
@@ -7,8 +11,9 @@
   <?php get_search_form(); ?>
 <?php endif; ?>
 
+<!-- <?php query_posts(array('post_type' => 'page','post_parent'=>get_the_ID())); ?> -->
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+  <?php get_template_part('templates/content-page', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
 <?php endwhile; ?>
 
 <?php the_posts_navigation(); ?>
